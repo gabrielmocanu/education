e17d83db950c2fafc87b384550294fe56b453456
Alydume <alexandru.dumenica@gmail.com>
2022-06-20 09:50:46 +0300

    myStrlen

--- summer-2022/1-intro/activities/my-strlen/my-strlen.c
+++ summer-2022/1-intro/activities/my-strlen/my-strlen.c
@@ -3,3 +3,20 @@
 Write a function myStrLen(char*) which returns the length of the parameter cstring.  Write the function without using the C++ function strlen.
 
 */
+#include<stdio.h>
+
+int myStrLen(char* a){
+    int len;
+    len = 0;
+    while(*a){
+        len++;
+        a++;
+    }
+    return len;
+}
+
+int main(){
+    char s[200] = {0};
+    scanf("%s", s);
+    printf("%d\n", myStrLen(s));
+}
